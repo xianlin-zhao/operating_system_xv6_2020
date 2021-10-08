b15a95881022a5c595a00a6ca710725f1b6c9a51
xianlin-zhao <1900013063@pku.edu.cn>
2021-10-08 16:39:32 +0800

    finish all exercises

--- user/uthread.c
+++ user/uthread.c
@@ -10,10 +10,29 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct info {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct info info;
 
 };
 struct thread all_thread[MAX_THREAD];
@@ -63,6 +82,8 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)(&(t->info)), (uint64)(&(next_thread->info)));
+
   } else
     next_thread = 0;
 }
@@ -77,6 +98,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  (&(t->info))->ra = (uint64)func;
+  (&(t->info))->s0 = (uint64)(&(t->stack[STACK_SIZE - 1]));
+  (&(t->info))->sp = (uint64)(&(t->stack[STACK_SIZE - 1]));
 }
 
 void 
